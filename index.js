0211a78be7c124c9bb040f16ddaee77d8ca419da
Lê Tuấn Anh <letuananh30@gmail.com>
2019-06-06 17:21:07 +0700

    update index

--- index.js
+++ index.js
@@ -173,7 +173,110 @@ mongoose.connection.on('open', function (ref) {
     });
 })
 //================================#END GENERATE DATA_PROVIDER========================================
+function genrateEntity(collectionName, listKey, listType) {
+    var cmd1 = 'mkdir core entity';
+    var cmd2 = 'mv entity core';
+    IO.execute(cmd1, cmd2);
+    _.each(collectionName, (item) => {
+        db2 = 'touch' + ' ' + 'core/entity/' + item + '.js'
+        IO.execute(db2)
+        typeList = _.rest(_.map(listKey, (k) => {
+            return k + ': ' + IO.mongoTypeOf(listType[k])
+        }))
+        let demoCoreEntity = fs.readFileSync('demo/entity.js').toString()
+            .replace(new RegExp('NAMESCHEMA', 'g'), IO.toProperCase(item) + 'Schema')
+            .replace(new RegExp('ENTITY', 'g'), IO.toProperCase(item))
+            .replace('NAMEDB', item)
+            .replace('ENTITNAME', IO.toProperCase(item) + 'Entity')
+            .replace('TYPE', typeList)
+        fs.writeFileSync('core/entity/' + item + '.js', demoCoreEntity.toString());
+    })
+    typeList = _.map(collectionName, (item) => {
+        return (IO.toProperCase(item) + ' = ' + " require('" + './' + item + "'" + ')')
+    })
+    router = _.map(collectionName, (item) => {
+        return 'exports.' + IO.toProperCase(item) + 'Entity' + ' = ' + IO.toProperCase(item) + '.' + IO.toProperCase(item) + 'Entity;'
+    })
+    router = router.toString().replace(/;,/g, ';')
+    let demoIndex = fs.readFileSync('demo/coreEntiryIndex.js').toString()
+        .replace('[[ROUTES]]', typeList)
+        .replace('[[ROUTESENTITY]]', router)
+    fs.writeFileSync('core/entity/index.js', demoIndex.toString());
+}
 
-setTimeout((function () {
-    return process.abort();
-}), 5000);
+function genrateUsecase(collectionName) {
+    var cmd1 = 'mkdir core use_case';
+    var cmd2 = 'mv use_case core';
+    IO.execute(cmd1, cmd2);
+    _.each(collectionName, (item) => {
+        db = 'touch' + ' ' + 'core/use_case/' + item + '.js'
+        IO.execute(db)
+    })
+    _.each(collectionName, (item) => {
+        let demoCoreUsercase = fs.readFileSync('demo/use_case.js').toString()
+            .replace('DBNAME', item)
+            .replace(new RegExp('NAMEID', 'g'), item + 'Id')
+        fs.writeFileSync('core/use_case/' + item + '.js', demoCoreUsercase.toString());
+    })
+}
+
+function genrateRoutes(collectionName) {
+    var cmd = 'mkdir routes';
+    IO.execute(cmd);
+    _.each(collectionName, (item) => {
+        db = 'touch' + ' ' + 'routes' + '/' + item + '.js'
+        IO.execute(db)
+    })
+    _.each(collectionName, (item) => {
+        let demoRoutes = fs.readFileSync('demo/routes.js').toString()
+            .replace(new RegExp('DBNAME', 'g'), item)
+            .replace(new RegExp('NAMEID', 'g'), item + 'Id')
+        fs.writeFileSync('routes/' + item + '.js', demoRoutes.toString());
+    })
+    typeList = _.map(collectionName, (item) => {
+        return (IO.toProperCase(item) + ' = ' + " require('" + './' + item + "'" + ')')
+    })
+    router = _.map(collectionName, (item) => {
+        return "app.post('/" + item + "/getAll'," + IO.toProperCase(item) + ".getAll);" +
+            "app.post('/" + item + "/getById'," + IO.toProperCase(item) + ".getById);" +
+            "app.post('/" + item + "/create'," + IO.toProperCase(item) + ".create);" +
+            "app.post('/" + item + "/delete'," + IO.toProperCase(item) + ".delete);" +
+            "app.post('/" + item + "/update'," + IO.toProperCase(item) + ".update);"
+    })
+    router = router.toString().replace(/;,/g, ';')
+    let demoIndex = fs.readFileSync('demo/routesIndex.js').toString()
+        .replace('[[ROUTES]]', typeList)
+        .replace('[[ROUTESAPI]]', router)
+    fs.writeFileSync('routes/index.js', demoIndex.toString());
+}
+
+function genrateDataprovider(collectionName, listKey, listType) {
+    var cmd = 'mkdir data_provider';
+    IO.execute(cmd);
+    _.each(collectionName, (item) => {
+        db3 = 'touch' + ' ' + 'data_provider' + '/' + item + '.js'
+        IO.execute(db3)
+    })
+    _.each(collectionName, (item) => {
+        typeList = _.rest(_.map(listKey, (k) => {
+            if (IO.mongoTypeOf(listType[k]) == 'ObjectId') {
+                return "'" + k + "'" + ': ' + ' mongoose.Types.ObjectId' + '(data.' + k + ')'
+            }
+            return "'" + k + "'" + ': ' + 'data.' + k
+        }))
+        update = _.rest(_.map(listKey, (k) => {
+            if (IO.mongoTypeOf(listType[k]) == 'ObjectId') {
+                return '((' + 'data.' + k + ' ==' + "''" + ' ||' + ' data.' + k + ' ==' + ' undefined)' + ' ?' + ' result[0].' + k + ' :' + ' mongoose.Types.ObjectId' + ' (data.' + k + '))'
+            }
+            return '((' + 'data.' + k + ' ==' + "''" + ' ||' + ' data.' + k + ' ==' + ' undefined)' + ' ?' + ' result[0].' + k + ' :' + ' data.' + k + ')'
+        }))
+        update = update.toString().replace(/,/g, ';')
+        let demoDataprovider = fs.readFileSync('demo/data_provider.js').toString()
+            .replace(new RegExp('DBENTITY', 'g'), IO.toProperCase(item) + 'Entity')
+            .replace(new RegExp('NAMEID', 'g'), item + 'Id')
+            .replace(new RegExp('DBNAME', 'g'), IO.toProperCase(item))
+            .replace(new RegExp('DATA', 'g'), typeList)
+            .replace('UPDATE', update)
+        fs.writeFileSync('data_provider/' + item + '.js', demoDataprovider.toString());
+    })
+}
