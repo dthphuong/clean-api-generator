e37c5f38690f5859e89a6b8ed179e8c916209039
Phuong Duong <dthphuong94@live.com>
2019-06-08 00:26:46 +0700

    make new UI

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+/**
+ * Created by Phuong Duong on 07/06/2019
+ */
 global._ = require('underscore');
+global.utils = require('./utils')
 
 const MongoClient = require('mongodb').MongoClient;
 const readline = require('readline');
 const async = require('async');
 const chalk = require('chalk');
+const figlet = require('figlet');
 
-var prompt = chalk.bold.green;
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -14,14 +19,23 @@ const rl = readline.createInterface({
 // ============================================================================================
 // ======================================DEFAULT VARIABLE======================================
 // ============================================================================================
+var ask = chalk.bold.red,
+    info = chalk.italic.white,
+    comment = chalk.bold.bgBlue.white;
+
 var inputData = {
     // project variables
     name: 'demo-clean-api',
     version: '1.0.0',
     description: 'This is Demo API based on Clean architecture',
     main: 'server.js',
-    keyword: 'fpo,api',
+    repo: '',
+    keywords: [
+        'fpo',
+        'api'
+    ],
     author: 'FPO Co.,Ltd',
+    license: 'ISC',
 
     // database variables
     host: 'localhost',
@@ -41,18 +55,104 @@ var inputData = {
 // ============================================================================================
 console.clear();
 
+console.log(figlet.textSync('Clean  API  Generator', {
+    kerning: 'full'
+}));
 
+console.log(info('This utility will walk you through creating an API that based on Clean Architecture with Node.JS and Express.JS'));
+console.log(info('Developed by FPO Co.,Ltd'));
+console.log(info('Email: contact@fpo.vn'));
+console.log(info('\n'));
+console.log(info('Press Ctrl + C at any time to quit.\n'));
 
 async.series([
     (callback) => {
-        rl.question(prompt('Question 1: '), (answer) => {
-            inputData.ques1 = answer
+        console.log(comment('______________________________________________________'));
+        console.log(comment('____________________Project config____________________'));
+        console.log(comment('______________________________________________________'));
+        rl.question(ask('📒 project name: (demo-clean-api) '), (answer) => {
+            inputData.name = (answer == '') ? inputData.name : answer
             callback()
         })
     },
     (callback) => {
-        rl.question(prompt('Question 2: '), (answer) => {
-            inputData.ques2 = answer
+        rl.question(ask('1️⃣ version: (1.0.0) '), (answer) => {
+            inputData.version = (answer == '') ? inputData.version : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('📃 description: '), (answer) => {
+            inputData.description = (answer == '') ? inputData.description : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('📌 run file: (server.js) '), (answer) => {
+            inputData.main = (answer == '') ? inputData.main : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('📦 git repository: '), (answer) => {
+            inputData.repo = (answer == '') ? inputData.repo : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('🏷  keywords: '), (answer) => {
+            inputData.keywords = (answer == '') ? inputData.keywords : utils.String.trimKeywords(answer)
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('👤 author: '), (answer) => {
+            inputData.author = (answer == '') ? inputData.author : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('🗝️  license: (ISC) '), (answer) => {
+            inputData.license = (answer == '') ? inputData.license : answer
+            callback()
+        })
+    },
+    (callback) => {
+        console.log(comment('_______________________________________________________'));
+        console.log(comment('____________________Database config____________________'));
+        console.log(comment('_______________________________________________________'));
+        rl.question(ask('🌐 host: (localhost) '), (answer) => {
+            inputData.host = (answer == '') ? inputData.host : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('🚪 port: (27017) '), (answer) => {
+            inputData.port = (answer == '') ? inputData.port : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('🗂 dbName: (demo) '), (answer) => {
+            inputData.dbName = (answer == '') ? inputData.dbName : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('👤 username: '), (answer) => {
+            inputData.username = (answer == '') ? inputData.username : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('🔐 password: '), (answer) => {
+            inputData.password = (answer == '') ? inputData.password : answer
+            callback()
+        })
+    },
+    (callback) => {
+        rl.question(ask('ℹ️ optional: '), (answer) => {
+            inputData.optional = (answer == '') ? inputData.optional : answer
             callback()
         })
     }
