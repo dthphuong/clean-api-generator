eb91f972f42980d39037cb13c53394a63c2d28cb
Duong Tran Ha Phuong <phuongduong@fpo.vn>
2020-08-05 16:01:04 +0700

    update server template

--- core/template/server.js
+++ core/template/server.js
@@ -9,10 +9,7 @@ global.config = require("./config");
 global._ = require("underscore");
 var express = require("express"),
   bodyParser = require("body-parser"),
-  util = require('./utils'),
-  jwt = require("./utils/JWT"),
-  limiter = require('./utils/Limiter'),
-  fs = require('fs');
+  limiter = require('./utils/Limiter');
 var app = express();
 
 //#region Middleware
@@ -22,45 +19,12 @@ app.use('/oauth', limiter.mediumLimiter); // apply to oauth, decode,. . . api
 app.use('/system/decode', limiter.mediumLimiter); // apply to oauth, decode,. . . api
 
 app.use(bodyParser.json({
-  limit: "5000mb"
+  limit: "500mb"
 }));
 app.use(bodyParser.urlencoded({
-  limit: "5000mb",
+  limit: "500mb",
   extended: true
 }));
-
-app.use(function (req, res, next) {
-  console.log(req.rateLimit); // logging count limit
-
-  if (_.indexOf(config.server.noTokenUrls, req.originalUrl) == -1) {
-    // do not need a TOKEN
-    var token = req.body.token || req.headers["x-access-token"];
-
-    if (token) {
-      var decoded = jwt.decode(token);
-
-      if (decoded.exitcode != 1) {
-        //fail
-        return res.json({
-          exitcode: decoded.exitcode,
-          data: '',
-          message: decoded.error
-        });
-      } else {
-        req.payload = decoded;
-        next();
-      }
-    } else {
-      return res.status(403).send({
-        exitcode: 0,
-        data: '',
-        message: "No token provided."
-      });
-    }
-  } else {
-    next();
-  }
-});
 
 //#endregion
 
