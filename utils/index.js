f28712c259f4090b4484f0624ee939955aa011c3
Duong Tran Ha Phuong <phuongduong@fpo.vn>
2021-09-29 13:33:16 +0700

    ⚡️ upgrade generator core library

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by Phuong Duong on 07/06/2019
  */
-exports.String = require('./String');
-exports.Type = require('./Type');
-exports.ErrorHandle = require('./ErrorHandle');
+exports.String = require('./IO')
+exports.Type = require('./Type')
+exports.ErrorHandle = require('./ErrorHandle')
